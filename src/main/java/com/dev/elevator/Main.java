5ee7238d2ff6d8e7f3bc9f88820bc62376ea89a5
Rostislav Levadnyi <rlevadniy@ukr.net>
2020-07-06 16:25:13 +0300

    class Main was deleted

--- src/main/java/com/dev/elevator/Main.java
+++
@@ -1,8 +0,0 @@
-package com.dev.elevator;
-
-public class Main {
-    public static void main(String[] args) {
-        int[][] arr = new int[][] {{}, {6,5,2}, {4}, {}, {0,0,0}, {}, {}, {3,6,4,5,6}, {}, {1,10,2}, {1,4,3,2}};
-        Elevator.theLift(arr, 5);
-    }
-}
